e0e7ac238cdb9855ee3f6043990663d8e1d020c0
adamstaylordev <50116317+adamstaylordev@users.noreply.github.com>
2019-04-29 13:12:02 -0700

    Add files via upload

---
+++ html/india-digital-marketing-campaign/old/general-landing-page/js/custom.js
@@ -0,0 +1,67 @@
+$(document).on('ready', function() {
+ 
+ $('.scrolltop').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+
+
+$('.global-slider').slick({
+  slidesToShow: 7,
+  responsive: [
+   
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 4,
+
+      }
+      },
+	   
+     {
+      breakpoint: 639,
+      settings: {
+        slidesToShow: 3,
+      }
+      },
+  
+      {
+      breakpoint: 479,
+      settings: {
+        slidesToShow: 2,
+      }
+ 
+	  
+    }
+  ]
+});
+
+
+$('.world-slider').slick({
+  slidesToShow: 3,
+  arrows: true,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 979,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        centerPadding: '0px',
+        slidesToShow: 1,
+      }
+    }
+  ]
+});
+
+
+AOS.init();
+
+});
